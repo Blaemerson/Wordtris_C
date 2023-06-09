f6540dcc66f7abe4608c88b8a613f207b138eb9d
Blaemerson <blakesavage99@gmail.com>
2023-06-09 10:51:19 -0500

    Use prefix trie to store dictionary

--- main.c
+++ main.c
@@ -5,7 +5,9 @@
 #include <stdlib.h>
 #include <string.h>
 
-#include "words.c"
+#include "trie.c"
+
+// #include "words.c"
 
 #define GRID_WIDTH 6
 #define GRID_HEIGHT 10
@@ -16,7 +18,54 @@
 #define SCREEN_WIDTH (GRID_WIDTH * TILE_SIZE) + (4 * TILE_SIZE)
 #define SCREEN_HEIGHT (GRID_HEIGHT * TILE_SIZE)
 
-char _letter_probs[] = {
+typedef enum TileState {
+    FALLING,
+    STATIC,
+    EMPTY,
+} TileState;
+
+typedef struct Tile {
+    TileState state;
+    char letter;
+} Tile;
+
+typedef enum GameState {
+    GAMEOVER,
+    PLAYING,
+    PAUSED,
+} GameState;
+
+typedef enum Spin {
+    COUNTER_CLOCKW,
+    CLOCKW,
+} Spin;
+
+typedef enum PlayerRotation {
+    ROT_0,
+    ROT_1,
+    ROT_2,
+    ROT_3,
+} PlayerRotation;
+
+typedef struct Player {
+    Tile *tiles[2];
+    int indexes[2];
+    PlayerRotation rotation;
+} Player;
+
+typedef struct SoundEffects {
+    Sound move_success;
+    Sound move_failure;
+    Sound word_found;
+} SoundEffects;
+
+GameState _state;
+SoundEffects _sfx;
+Tile _grid[GRID_WIDTH * GRID_HEIGHT];
+Player _player;
+Font _font;
+
+char _letterProbs[] = {
     'A', 'A', 'A', 'A', 'A', 'A', 'B', 'B', 'C', 'C', 'C', 'D', 'D', 'D', 'D',
     'E', 'E', 'E', 'E', 'E', 'E', 'F', 'F', 'F', 'G', 'G', 'H', 'H', 'I', 'I',
     'I', 'I', 'I', 'J', 'K', 'K', 'L', 'L', 'L', 'L', 'M', 'M', 'M', 'O', 'O',
@@ -24,342 +73,276 @@ char _letter_probs[] = {
     'T', 'T', 'U', 'U', 'U', 'U', 'U', 'V', 'V', 'W', 'W', 'X', 'Y', 'Y', 'Z',
 };
 
-bool bin_search(char *target) {
-  int number_words = sizeof(WORDS) / sizeof(WORDS[0]);
-  int bottom = 0;
-  int middle;
-  int top = number_words - 1;
-
-  while (bottom <= top) {
-    middle = (bottom + top) / 2;
-    if (strcmp(WORDS[middle], target) == 0) {
-      return true;
-    } else if (strcmp(WORDS[middle], target) > 0) {
-      top = middle - 1;
-    } else if (strcmp(WORDS[middle], target) < 0) {
-      bottom = middle + 1;
+void initGrid() {
+    for (int i = 0; i < NUM_TILES; i++) {
+        _grid[i].state = EMPTY;
+        _grid[i].letter = ' ';
     }
-  }
-  return false;
 }
 
-typedef enum TileState {
-  FALLING,
-  STATIC,
-  EMPTY,
-} TileState;
-
-typedef struct Tile {
-  TileState state;
-  char letter;
-} Tile;
-
-typedef struct Grid {
-  Tile tiles[GRID_WIDTH * GRID_HEIGHT];
-} Grid;
-
-typedef enum GameState {
-  GAMEOVER,
-  PLAYING,
-  PAUSED,
-} GameState;
-
-typedef enum Spin {
-  COUNTER_CLOCKW,
-  CLOCKW,
-} Spin;
-
-typedef enum PlayerRotation {
-  ROT_0,
-  ROT_1,
-  ROT_2,
-  ROT_3,
-} PlayerRotation;
-
-typedef struct Player {
-  Tile *tiles[2];
-  int indexes[2];
-  PlayerRotation rotation;
-} Player;
-
-typedef struct SoundEffects {
-  Sound move_success;
-  Sound move_failure;
-  Sound word_found;
-} SoundEffects;
-
-GameState _state;
-SoundEffects _sfx;
-Grid _grid;
-Player _player;
-Font _font;
-
-void init_grid() {
-  for (int i = 0; i < NUM_TILES; i++) {
-    _grid.tiles[i].state = EMPTY;
-    _grid.tiles[i].letter = ' ';
-  }
+void initSounds() {
+    _sfx.move_success = LoadSound("resources/move.wav");
+    _sfx.move_failure = LoadSound("resources/stuck.wav");
+    _sfx.word_found = LoadSound("resources/correct.wav");
 }
 
-void init_sounds() {
-  _sfx.move_success = LoadSound("resources/move.wav");
-  _sfx.move_failure = LoadSound("resources/stuck.wav");
-  _sfx.word_found = LoadSound("resources/correct.wav");
+void initGame() {
+    _state = PLAYING;
+    initGrid();
+    initSounds();
 }
 
-void init_game() {
-  _state = PLAYING;
-  init_grid();
-  init_sounds();
-}
+// void find_substrs(char *str) {
+//     int n = strlen(str);
+//
+//     for (int i = 0; i < n; i++) {
+//         char temp[n - i + 1];
+//         int tempindex = 0;
+//         for (int j = i; j < n; j++) {
+//             temp[tempindex++] = tolower(str[j]);
+//             temp[tempindex] = '\0';
+//             if (strlen(temp) > 2 && bin_search(temp)) {
+//                 printf("word found: %s\n", temp);
+//             }
+//         }
+//     }
+// }
 
-void find_substrs(char *str) {
-  int n = strlen(str);
+// void scan_game_board() {
+//     char line[GRID_WIDTH];
+//     for (int row = 0; row < GRID_HEIGHT; row++) {
+//         for (int x = GRID_WIDTH * row; x < GRID_WIDTH * (row + 1); x++) {
+//             line[x - (GRID_WIDTH * row)] = _grid[x].letter;
+//         }
+//
+//         find_substrs(line);
+//     }
+//
+//     char v_line[GRID_HEIGHT];
+//     for (int x = 0; x < GRID_WIDTH; x++) {
+//         for (int y = 0; y < NUM_TILES; y += GRID_WIDTH) {
+//             v_line[y / GRID_WIDTH] = _grid[x + y].letter;
+//         }
+//
+//         find_substrs(v_line);
+//     }
+// }
 
-  for (int i = 0; i < n; i++) {
-    char temp[n - i + 1];
-    int tempindex = 0;
-    for (int j = i; j < n; j++) {
-      temp[tempindex++] = tolower(str[j]);
-      temp[tempindex] = '\0';
-      if (strlen(temp) > 2 && bin_search(temp)) {
-        printf("word found: %s\n", temp);
-      }
+void drawGameBoard() {
+    for (int y = 0; y < GRID_HEIGHT; y++) {
+        for (int x = 0; x < GRID_WIDTH; x++) {
+            int i = y * GRID_WIDTH + x;
+            if (_grid[i].state == EMPTY) {
+                DrawRectangle(x * TILE_SIZE, y * TILE_SIZE, TILE_SIZE, TILE_SIZE, GRAY);
+                DrawRectangle(x * TILE_SIZE + 1, y * TILE_SIZE + 1, TILE_SIZE - 2,
+                              TILE_SIZE - 2, WHITE);
+                continue;
+            }
+
+            DrawRectangle(x * TILE_SIZE, y * TILE_SIZE, TILE_SIZE, TILE_SIZE, BLACK);
+            DrawRectangle(x * TILE_SIZE + 1, y * TILE_SIZE + 1, TILE_SIZE - 2,
+                          TILE_SIZE - 2, LIGHTGRAY);
+            Vector2 letterSize =
+                MeasureTextEx(_font, &_grid[i].letter, TILE_SIZE, 0);
+            DrawTextEx(_font, &_grid[i].letter,
+                       (Vector2){x * TILE_SIZE + ((TILE_SIZE - letterSize.x) / 2),
+                       y * TILE_SIZE + ((TILE_SIZE - letterSize.y) / 2)},
+                       TILE_SIZE, 0, BLACK);
+        }
     }
-  }
 }
 
-void scan_game_board() {
-  char line[GRID_WIDTH];
-  for (int row = 0; row < GRID_HEIGHT; row++) {
-    for (int x = GRID_WIDTH * row; x < GRID_WIDTH * (row + 1); x++) {
-      line[x - (GRID_WIDTH * row)] = _grid.tiles[x].letter;
+void stopBlock(Tile *t) {
+    if (t) {
+        t->state = STATIC;
+    }
+}
+
+void spawnPlayer() {
+    // set current player block to static
+    stopBlock(_player.tiles[0]);
+    stopBlock(_player.tiles[1]);
+
+    int l_idx = (GRID_WIDTH / 2) - 1;
+    int r_idx = l_idx + 1;
+
+    Tile* l_tile = &_grid[l_idx];
+    Tile* r_tile = &_grid[r_idx];
+
+    bool can_spawn = l_tile->state == EMPTY && r_tile->state == EMPTY;
+    if (can_spawn) {
+        _player.tiles[0] = l_tile;
+        _player.tiles[1] = r_tile;
+        _player.indexes[0] = l_idx;
+        _player.indexes[1] = r_idx;
+        _player.tiles[0]->state = FALLING;
+        _player.tiles[1]->state = FALLING;
+        _player.rotation = ROT_0;
+        _player.tiles[0]->letter = _letterProbs[GetRandomValue(0, 74)];
+        _player.tiles[1]->letter = _letterProbs[GetRandomValue(0, 74)];
+    } else {
+        _state = GAMEOVER;
+    }
+}
+
+bool checkCollision(int index, int tileIndex) {
+
+    bool moving_down = (index == tileIndex + GRID_WIDTH);
+    bool moving_right = !moving_down && (index > tileIndex);
+    bool moving_up = (index == tileIndex - GRID_WIDTH);
+    bool moving_left = !moving_up && (index < tileIndex);
+
+    bool collided = false;
+
+    if (moving_down) {
+        collided = tileIndex > (GRID_WIDTH * (GRID_HEIGHT - 1) - 1);
+    } else if (moving_right) {
+        collided = tileIndex % GRID_WIDTH == GRID_WIDTH - 1;
+    } else if (moving_up) {
+        collided = tileIndex < GRID_WIDTH;
+    } else if (moving_left) {
+        collided = tileIndex % GRID_WIDTH == 0;
     }
 
-    find_substrs(line);
-  }
-
-  char v_line[GRID_HEIGHT];
-  for (int x = 0; x < GRID_WIDTH; x++) {
-    for (int y = 0; y < NUM_TILES; y += GRID_WIDTH) {
-      v_line[y / GRID_WIDTH] = _grid.tiles[x + y].letter;
-    }
-
-    find_substrs(v_line);
-  }
+    return (!collided && _grid[index].state != STATIC);
 }
 
-void draw_game_board() {
-  for (int y = 0; y < GRID_HEIGHT; y++) {
-    for (int x = 0; x < GRID_WIDTH; x++) {
-      int i = y * GRID_WIDTH + x;
-      if (_grid.tiles[i].state == EMPTY) {
-        DrawRectangle(x * TILE_SIZE, y * TILE_SIZE, TILE_SIZE, TILE_SIZE, GRAY);
-        DrawRectangle(x * TILE_SIZE + 1, y * TILE_SIZE + 1, TILE_SIZE - 2,
-                      TILE_SIZE - 2, WHITE);
-        continue;
-      }
-
-      DrawRectangle(x * TILE_SIZE, y * TILE_SIZE, TILE_SIZE, TILE_SIZE, BLACK);
-      DrawRectangle(x * TILE_SIZE + 1, y * TILE_SIZE + 1, TILE_SIZE - 2,
-                    TILE_SIZE - 2, LIGHTGRAY);
-      Vector2 letterSize =
-          MeasureTextEx(_font, &_grid.tiles[i].letter, TILE_SIZE, 0);
-      DrawTextEx(_font, &_grid.tiles[i].letter,
-                 (Vector2){x * TILE_SIZE + ((TILE_SIZE - letterSize.x) / 2),
-                           y * TILE_SIZE + ((TILE_SIZE - letterSize.y) / 2)},
-                 TILE_SIZE, 0, BLACK);
-    }
-  }
-}
-
-void stop_blk(Tile *t) {
-  if (t) {
-    t->state = STATIC;
-  }
-}
-
-void spawn_player() {
-  // set current player block to static
-  stop_blk(_player.tiles[0]);
-  stop_blk(_player.tiles[1]);
-
-  int l_idx = (GRID_WIDTH / 2) - 1;
-  int r_idx = (GRID_WIDTH / 2);
-  if (_grid.tiles[l_idx].state == EMPTY && _grid.tiles[r_idx].state == EMPTY) {
-
-    _player.tiles[0] = &_grid.tiles[l_idx];
-    _player.tiles[1] = &_grid.tiles[r_idx];
-    _player.indexes[0] = l_idx;
-    _player.indexes[1] = r_idx;
-    _player.tiles[0]->state = FALLING;
-    _player.tiles[1]->state = FALLING;
-    _player.rotation = ROT_0;
-    _player.tiles[0]->letter = _letter_probs[GetRandomValue(0, 74)];
-
-    // prevent two of the same letter being chosen
-    do {
-      _player.tiles[1]->letter = _letter_probs[GetRandomValue(0, 74)];
-    } while (_player.tiles[0]->letter == _player.tiles[1]->letter);
-
-  } else {
-    _state = GAMEOVER;
-  }
-}
-
-bool check_collision(int index, int tileIndex) {
-
-  bool moving_down = (index == tileIndex + GRID_WIDTH);
-  bool moving_right = !moving_down && (index > tileIndex);
-  bool moving_up = (index == tileIndex - GRID_WIDTH);
-  bool moving_left = !moving_up && (index < tileIndex);
-
-  bool collided = false;
-
-  if (moving_down) {
-    collided = tileIndex > (GRID_WIDTH * (GRID_HEIGHT - 1) - 1);
-  } else if (moving_right) {
-    collided = tileIndex % GRID_WIDTH == GRID_WIDTH - 1;
-  } else if (moving_up) {
-    collided = tileIndex < GRID_WIDTH;
-  } else if (moving_left) {
-    collided = tileIndex % GRID_WIDTH == 0;
-  }
-
-  return (!collided && _grid.tiles[index].state != STATIC);
-}
-
-void unset_tile(Tile *t) {
-  t->state = EMPTY;
-  t->letter = ' ';
+void unsetTile(Tile *t) {
+    t->state = EMPTY;
+    t->letter = ' ';
 }
 
 void set_player(int i0, int i1) {
-  Tile tmp0 = *_player.tiles[0];
-  Tile tmp1 = *_player.tiles[1];
-  unset_tile(_player.tiles[0]);
-  unset_tile(_player.tiles[1]);
-  _player.indexes[0] = i0;
-  _player.indexes[1] = i1;
-  _grid.tiles[i0] = tmp0;
-  _grid.tiles[i1] = tmp1;
-  _player.tiles[0] = &_grid.tiles[i0];
-  _player.tiles[1] = &_grid.tiles[i1];
+    Tile tmp0 = *_player.tiles[0];
+    Tile tmp1 = *_player.tiles[1];
+    unsetTile(_player.tiles[0]);
+    unsetTile(_player.tiles[1]);
+    _player.indexes[0] = i0;
+    _player.indexes[1] = i1;
+    _grid[i0] = tmp0;
+    _grid[i1] = tmp1;
+    _player.tiles[0] = &_grid[i0];
+    _player.tiles[1] = &_grid[i1];
 }
 
-bool move_player(int dir_x, int dir_y) {
-  int i0 = _player.indexes[0] + dir_x + (dir_y * GRID_WIDTH);
-  int i1 = _player.indexes[1] + dir_x + (dir_y * GRID_WIDTH);
-  if (check_collision(i0, _player.indexes[0]) &&
-      check_collision(i1, _player.indexes[1])) {
+bool movePlayer(int dir_x, int dir_y) {
+    int i0 = _player.indexes[0] + dir_x + (dir_y * GRID_WIDTH);
+    int i1 = _player.indexes[1] + dir_x + (dir_y * GRID_WIDTH);
+    if (checkCollision(i0, _player.indexes[0]) &&
+        checkCollision(i1, _player.indexes[1])) {
 
-    set_player(i0, i1);
-    return true;
-  }
+        set_player(i0, i1);
+        return true;
+    }
 
-  return false;
+    return false;
 }
 
-bool rotate_player(Spin spin) {
-  int i0 = _player.indexes[0];
-  int i1 = _player.indexes[1];
-  PlayerRotation rot = _player.rotation;
+bool rotatePlayer(Spin spin) {
+    int i0 = _player.indexes[0];
+    int i1 = _player.indexes[1];
+    PlayerRotation rot = _player.rotation;
 
-  if ((rot == ROT_0 && spin == COUNTER_CLOCKW) ||
-      (rot == ROT_3 && spin == CLOCKW)) {
-    i0 += -GRID_WIDTH;
-    i1 += -1;
-    rot = spin == COUNTER_CLOCKW ? ROT_1 : ROT_2;
-  } else if ((rot == ROT_1 && spin == COUNTER_CLOCKW) ||
-             (rot == ROT_0 && spin == CLOCKW)) {
-    i0 += 1;
-    i1 += -GRID_WIDTH;
-    rot = spin == COUNTER_CLOCKW ? ROT_2 : ROT_3;
-  } else if ((rot == ROT_2 && spin == COUNTER_CLOCKW) ||
-             (rot == ROT_1 && spin == CLOCKW)) {
-    i0 += GRID_WIDTH;
-    i1 += 1;
-    rot = spin == COUNTER_CLOCKW ? ROT_3 : ROT_0;
-  } else if ((rot == ROT_3 && spin == COUNTER_CLOCKW) ||
-             (rot == ROT_2 && spin == CLOCKW)) {
+    if ((rot == ROT_0 && spin == COUNTER_CLOCKW) ||
+        (rot == ROT_3 && spin == CLOCKW)) {
+            i0 += -GRID_WIDTH;
+            i1 += -1;
+            rot = spin == COUNTER_CLOCKW ? ROT_1 : ROT_2;
+        } else if ((rot == ROT_1 && spin == COUNTER_CLOCKW) ||
+            (rot == ROT_0 && spin == CLOCKW)) {
+        i0 += 1;
+        i1 += -GRID_WIDTH;
+        rot = spin == COUNTER_CLOCKW ? ROT_2 : ROT_3;
+    } else if ((rot == ROT_2 && spin == COUNTER_CLOCKW) ||
+            (rot == ROT_1 && spin == CLOCKW)) {
+        i0 += GRID_WIDTH;
+        i1 += 1;
+        rot = spin == COUNTER_CLOCKW ? ROT_3 : ROT_0;
+    } else if ((rot == ROT_3 && spin == COUNTER_CLOCKW) ||
+            (rot == ROT_2 && spin == CLOCKW)) {
     i0 += -1;
     i1 += GRID_WIDTH;
     rot = spin == COUNTER_CLOCKW ? ROT_0 : ROT_1;
-  }
-
-  if (check_collision(i0, _player.indexes[0]) &&
-      check_collision(i1, _player.indexes[1])) {
-    _player.rotation = rot;
-    set_player(i0, i1);
-    return true;
-  }
-  return false;
 }
 
-void draw_frame() {
-  BeginDrawing();
+    if (checkCollision(i0, _player.indexes[0]) &&
+        checkCollision(i1, _player.indexes[1])) {
+        _player.rotation = rot;
+        set_player(i0, i1);
+        return true;
+    }
+    return false;
+}
 
-  ClearBackground(RAYWHITE);
-  draw_game_board();
+void drawFrame() {
+    BeginDrawing();
 
-  EndDrawing();
+    ClearBackground(RAYWHITE);
+    drawGameBoard();
+
+    EndDrawing();
 }
 
 int main() {
-  InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Wordtris");
-  InitAudioDevice();
-  SetTargetFPS(60);
-  init_game();
+    InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Wordtris");
+    InitAudioDevice();
+    SetTargetFPS(60);
+    initGame();
 
-  _font = LoadFontEx("./Arialbd.TTF", TILE_SIZE, 0, 0);
-  spawn_player();
+    _font = LoadFontEx("./Arialbd.TTF", TILE_SIZE, 0, 0);
+    spawnPlayer();
 
-  double tick_time = 1.0;
-  double time = GetTime();
-  while (!WindowShouldClose()) {
-    // in-game tick
-    if (GetTime() >= time + tick_time) {
-      time = GetTime();
-      if (!move_player(0, 1)) {
-        PlaySound(_sfx.move_failure);
-        scan_game_board();
-        spawn_player();
-      }
+    struct TrieNode* dictTrieRoot = createNode();
+    constructTrie(dictTrieRoot, "./dictionary.txt");
+
+    double tick_time = 1.0;
+    double time = GetTime();
+    while (!WindowShouldClose()) {
+        // in-game tick
+        if (GetTime() >= time + tick_time) {
+            time = GetTime();
+            if (!movePlayer(0, 1)) {
+                PlaySound(_sfx.move_failure);
+                // scan_game_board();
+                spawnPlayer();
+            }
+        }
+
+        switch (GetKeyPressed()) {
+            case (KEY_A):
+                movePlayer(-1, 0) ? PlaySound(_sfx.move_success)
+                    : PlaySound(_sfx.move_failure);
+                break;
+            case (KEY_D):
+                movePlayer(1, 0) ? PlaySound(_sfx.move_success)
+                    : PlaySound(_sfx.move_failure);
+                break;
+            case (KEY_S):
+                if (movePlayer(0, 1)) {
+                    time = GetTime();
+                    PlaySound(_sfx.move_success);
+                } else {
+                    time = tick_time;
+                }
+                break;
+            case (KEY_K):
+                rotatePlayer(COUNTER_CLOCKW);
+                break;
+            case (KEY_J):
+                rotatePlayer(CLOCKW);
+                break;
+        };
+
+        if (_state == GAMEOVER) {
+            break;
+        }
+        drawFrame();
     }
 
-    switch (GetKeyPressed()) {
-    case (KEY_A):
-      move_player(-1, 0) ? PlaySound(_sfx.move_success)
-                         : PlaySound(_sfx.move_failure);
-      break;
-    case (KEY_D):
-      move_player(1, 0) ? PlaySound(_sfx.move_success)
-                        : PlaySound(_sfx.move_failure);
-      break;
-    case (KEY_S):
-      if (move_player(0, 1)) {
-        time = GetTime();
-        PlaySound(_sfx.move_success);
-      } else {
-        time = tick_time;
-      }
-      break;
-    case (KEY_K):
-      rotate_player(COUNTER_CLOCKW);
-      break;
-    case (KEY_J):
-      rotate_player(CLOCKW);
-      break;
-    };
+    freeTrie(dictTrieRoot);
 
-    if (_state == GAMEOVER) {
-      break;
-    }
-    draw_frame();
-  }
+    UnloadFont(_font);
+    CloseWindow();
 
-  UnloadFont(_font);
-  CloseWindow();
-
-  return 0;
+    return 0;
 }
---
+++ trie.c
@@ -0,0 +1,94 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX_CHILDREN 26
+
+struct TrieNode {
+    struct TrieNode* children[MAX_CHILDREN]; // Array to store child nodes for each letter (assuming only lowercase alphabets)
+    bool isEndOfWord; // Flag to mark the end of a word
+};
+
+struct TrieNode* createNode() {
+    printf("createNoe\n");
+    struct TrieNode* node = (struct TrieNode*)malloc(sizeof(struct TrieNode));
+
+    for (int i = 0; i < MAX_CHILDREN; i++) {
+        node->children[i] = NULL;
+    }
+
+    node->isEndOfWord = false;
+
+    return node;
+}
+
+void freeTrie(struct TrieNode* node) {
+    if (node == NULL) {
+        return;
+    }
+
+    for (int i = 0; i < 26; i++) {
+        freeTrie(node->children[i]);
+    }
+
+    free(node);
+}
+
+void insertWord(struct TrieNode* root, const char* word) {
+    printf("insert\n");
+    struct TrieNode* curr = root;
+
+    for (int i = 0; word[i] != '\0'; i++) {
+        int index = word[i] - 'a'; // Convert character to index (assuming only lowercase alphabets)
+
+        if (curr->children[index] == NULL) {
+            curr->children[index] = createNode();
+        }
+
+        curr = curr->children[index];
+    }
+
+    curr->isEndOfWord = true;
+}
+
+int constructTrie(struct TrieNode* root, const char* dictionaryFile) {
+    printf("consruct\n");
+    FILE* file = fopen(dictionaryFile, "r");
+
+    if (file == NULL) {
+        printf("ERROR in constructTrie: unable to open dictionary file");
+        return -1;
+        // Handle error opening the dictionary file
+    }
+
+    char word[100];
+
+    while (fgets(word, sizeof(word), file) != NULL) {
+        // Remove newline character from the word, if present
+        word[strcspn(word, "\n")] = '\0';
+
+        insertWord(root, word);
+    }
+
+    fclose(file);
+
+    return 0;
+}
+
+bool searchWord(struct TrieNode* root, const char* word) {
+    struct TrieNode* curr = root;
+
+    for (int i = 0; word[i] != '\0'; i++) {
+        int index = word[i] - 'a'; // Convert character to index (assuming only lowercase alphabets)
+
+        if (curr->children[index] == NULL) {
+            return false; // Word does not exist
+        }
+
+        curr = curr->children[index];
+    }
+
+    return (curr != NULL && curr->isEndOfWord);
+}
+
